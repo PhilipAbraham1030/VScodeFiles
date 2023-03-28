adc3d19ee828a7a8d9c65f3960c8c54c6b08cc54
PHILIP ABRAHAM <58961005+PhilipAbraham1030@users.noreply.github.com>
2023-03-28 10:23:29 -0500

    Update basicVSpython1.py
    added some data to data

--- basicVSpython1.py
+++ basicVSpython1.py
@@ -3,7 +3,7 @@ print(2+2)
 import pandas as pd
 
 # initialize list elements
-data = [10,20,30,40,50,60]
+data = [10,20,30,40,50,60,90,100]
   
 # Create the pandas DataFrame with column name is provided explicitly
 df = pd.DataFrame(data, columns=['Numbers'])
