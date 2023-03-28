f8d8588fb47be625e30e2a9a7ca22ce31d7a9aa0
PhilipAbraham1030 <philipabraham1030@gmail.com>
2023-03-28 09:54:34 -0500

    First commit

---
+++ basicVSpython1.py
@@ -0,0 +1,24 @@
+print('Hello')
+print(2+2)
+import pandas as pd
+
+# initialize list elements
+data = [10,20,30,40,50,60]
+  
+# Create the pandas DataFrame with column name is provided explicitly
+df = pd.DataFrame(data, columns=['Numbers'])
+  
+# print dataframe.
+print(df)
+
+a=3
+
+print("hello you are a: "+str(a))
+
+print('you are a: ',a)
+
+print(5//2, 5/2, 5%2)
+
+list1 = [1,2,"a"]
+print(2*list1)
+
