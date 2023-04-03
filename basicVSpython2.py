ebc4051d9e0ee9c4ba157210ea1cf1d6dabaf63a
PhilipAbraham1030 <philipabraham1030@gmail.com>
2023-04-03 14:43:21 -0500

    made slight changes

--- basicVSpython2.py
+++ basicVSpython2.py
@@ -12,7 +12,9 @@ print(np.dot(vec1,vec2))
 print(np.cross(vec1,vec2))
 print(np.cross(vec2,vec1))
 
-# norm
+# norm and calcs
 vec3 = np.array([3,4])
 print(np.linalg.norm(vec3))
 print(np.sqrt(3**2 + 4**2))
+
+print(vec3)
